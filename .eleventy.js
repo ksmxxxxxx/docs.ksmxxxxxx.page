514b294bf31baafb429fbaa20443da30c80a9995
ksmxxxxxx <kankichi69@gmail.com>
2021-06-06 01:46:28 +0900

    :wrench: Copy images directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   const pug = require('pug')
   // eleventyConfig.setPugOptions({ debug: true })
   eleventyConfig.setLibrary('pug', pug)
+  eleventyConfig.addPassthroughCopy('_sources/images')
   return {
     dir: {
       input: '_sources',
