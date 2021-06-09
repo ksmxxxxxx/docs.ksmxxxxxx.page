4077779f729985f5c97ae5b23d34a5852d3cdff8
ksmxxxxxx <kankichi69@gmail.com>
2021-06-09 23:45:42 +0900

    :construction: Can see the pages
    - enable to Pug template engine
    - localize the dates with luxon

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,24 @@
+const { Settings, DateTime } = require('luxon')
+const pug = require('pug')
+
 module.exports = function (eleventyConfig) {
-  const pug = require('pug')
-  // eleventyConfig.setPugOptions({ debug: true })
-  eleventyConfig.setLibrary('pug', pug)
+  global.filters = eleventyConfig.javascriptFunctions
   eleventyConfig.addPassthroughCopy('_sources/images')
+  eleventyConfig.setLibrary('pug', pug)
+  eleventyConfig.setPugOptions({
+    filters: global.filters
+  })
+
+  Settings.defaultZoneName = 'Asia/Tokyo'
+
+  eleventyConfig.addFilter('htmlDateString', dateObj => {
+    return DateTime.fromJSDate(dateObj).toISO()
+  })
+
+  eleventyConfig.addFilter('readableDate', dataObj => {
+    return DateTime.fromJSDate(dataObj).toFormat('yyyy年MM月dd日 HH時mm分')
+  })
+
   return {
     dir: {
       input: '_sources',
