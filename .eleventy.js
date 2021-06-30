db0bdae2228eb0cc5424b189ab78812684476f57
ksmxxxxxx <kankichi69@gmail.com>
2021-06-30 23:14:27 +0900

    :put_litter_in_its_place: Do not set up to a 404 page
    redirect on the local server

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,8 @@
-const fs = require('fs')
 const { Settings, DateTime } = require('luxon')
 const pluginSass = require('eleventy-plugin-sass')
 const pug = require('pug')
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, bs) {
-        if (err) {
-          bs.addMiddlewere('*', (req, res) => {
-            const notFound404 = fs.readFileSync('_site/404.html')
-            res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
-            res.write(notFound404)
-            res.end()
-          })
-        }
-      }
-    }
-  })
-
   global.filters = eleventyConfig.javascriptFunctions
   eleventyConfig.addPassthroughCopy('_sources/images')
   eleventyConfig.setLibrary('pug', pug)
