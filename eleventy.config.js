4040ad9eb0312d851d2633c39a041886b622e4b0
ksmxxxxxx <kankichi69@gmail.com>
2024-12-30 17:21:15 +0900

    Upgrade eleventy

---
+++ eleventy.config.js
@@ -0,0 +1,78 @@
+import { Settings, DateTime } from 'luxon'
+import PluginSass from 'eleventy-sass'
+import UpgradeHelper from '@11ty/eleventy-upgrade-help'
+import pugPlugin from '@11ty/eleventy-plugin-pug'
+import { Liquid } from 'liquidjs'
+import markdownIt from 'markdown-it'
+import markdownItAnchor from 'markdown-it-anchor'
+import markdownItFootnote from 'markdown-it-footnote'
+import pluginTOC from 'eleventy-plugin-toc'
+import pluginRss from '@11ty/eleventy-plugin-rss'
+
+export default function (eleventyConfig) {
+  global.filters = eleventyConfig.javascriptFunctions
+
+  const markdownOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true
+  }
+
+  const markdownAnchorOptions = {
+    class: 'anchor-link',
+    symbol: '#'
+  }
+
+  const liquidOptions = {
+    extname: '.liquid',
+    dynamicPartials: false,
+    staticFilters: false,
+    root: ['_includes']
+  }
+
+  eleventyConfig.addPlugin(pugPlugin, {
+    filters: global.filters
+  })
+
+  eleventyConfig.addPlugin(UpgradeHelper)
+  eleventyConfig.addPlugin(PluginSass)
+
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginTOC, {
+    tags: ['h2', 'h3'],
+    wrapper: 'div',
+    ul: true
+  })
+
+  Settings.defaultZone = 'Asia/Tokyo'
+  eleventyConfig.addFilter('htmlDateString', (dateObj) =>
+    DateTime.fromJSDate(dateObj).toISO()
+  )
+  eleventyConfig.addFilter('readableDate', (dataObj) =>
+    DateTime.fromJSDate(dataObj).toFormat('yyyy年MM月dd日 HH時mm分')
+  )
+
+  eleventyConfig.addPassthroughCopy('_sources/images')
+  eleventyConfig.addPassthroughCopy('_sources/_redirects')
+  eleventyConfig.setLibrary('liquid', new Liquid(liquidOptions))
+  eleventyConfig.setLibrary(
+    'md',
+    markdownIt(markdownOptions)
+      .use(markdownItFootnote)
+      .use(markdownItAnchor, markdownAnchorOptions)
+  )
+
+  eleventyConfig.setLiquidOptions({ jsTruthy: true })
+
+  eleventyConfig.addCollection('articles', (collection) =>
+    collection.getFilteredByGlob('**/articles/*.md')
+  )
+
+  return {
+    dir: {
+      input: '_sources',
+      data: '_data'
+    }
+  }
+}
