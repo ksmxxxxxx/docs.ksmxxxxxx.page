dd6cda82e0d01e9be874dc2d88a766b42f3d5f31
ksmxxxxxx <kankichi69@gmail.com>
2025-04-27 14:09:51 +0900

    SNSの埋め込みができるようになる

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,68 +1,72 @@
-import { Settings, DateTime } from 'luxon'
-import { Liquid } from 'liquidjs'
-import markdownIt from 'markdown-it'
-import markdownItAnchor from 'markdown-it-anchor'
-import markdownItFootnote from 'markdown-it-footnote'
-import pluginTOC from 'eleventy-plugin-toc'
-import pluginRss from '@11ty/eleventy-plugin-rss'
+import { Settings, DateTime } from "luxon";
+import { Liquid } from "liquidjs";
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+import markdownItFootnote from "markdown-it-footnote";
+import pluginTOC from "eleventy-plugin-toc";
+import pluginRss from "@11ty/eleventy-plugin-rss";
+import embedEverything from "eleventy-plugin-embed-everything";
 
 export default function (eleventyConfig) {
-  global.filters = eleventyConfig.javascriptFunctions
+  global.filters = eleventyConfig.javascriptFunctions;
 
   const markdownOptions = {
     html: true,
     breaks: true,
     linkify: true,
-    typographer: true
-  }
+    typographer: true,
+  };
 
   const markdownAnchorOptions = {
-    class: 'anchor-link',
-    symbol: '#'
-  }
+    class: "anchor-link",
+    symbol: "#",
+  };
 
   const liquidOptions = {
-    extname: '.liquid',
+    extname: ".liquid",
     dynamicPartials: false,
     staticFilters: false,
-    root: ['_sources/_includes']
-  }
+    root: ["_sources/_includes"],
+  };
 
-  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginTOC, {
-    tags: ['h2', 'h3'],
-    wrapper: 'div',
-    ul: true
-  })
+    tags: ["h2", "h3"],
+    wrapper: "div",
+    ul: true,
+  });
 
-  Settings.defaultZone = 'Asia/Tokyo'
-  eleventyConfig.addFilter('htmlDateString', (dateObj) =>
+  eleventyConfig.addPlugin(embedEverything);
+
+  Settings.defaultZone = "Asia/Tokyo";
+  eleventyConfig.addFilter("htmlDateString", (dateObj) =>
     DateTime.fromJSDate(dateObj).toISO()
-  )
-  eleventyConfig.addFilter('readableDate', (dataObj) =>
-    DateTime.fromJSDate(dataObj).toFormat('yyyy年MM月dd日 HH時mm分')
-  )
+  );
 
-  eleventyConfig.addPassthroughCopy('_sources/images')
-  eleventyConfig.addPassthroughCopy('_sources/_redirects')
-  eleventyConfig.setLibrary('liquid', new Liquid(liquidOptions))
+  eleventyConfig.addFilter("readableDate", (dataObj) =>
+    DateTime.fromJSDate(dataObj).toFormat("yyyy年MM月dd日 HH時mm分")
+  );
+
+  eleventyConfig.addPassthroughCopy("_sources/images");
+  eleventyConfig.addPassthroughCopy("_sources/_redirects");
+  eleventyConfig.setLibrary("liquid", new Liquid(liquidOptions));
   eleventyConfig.setLibrary(
-    'md',
+    "md",
     markdownIt(markdownOptions)
       .use(markdownItFootnote)
       .use(markdownItAnchor, markdownAnchorOptions)
-  )
+  );
 
-  eleventyConfig.setLiquidOptions({ jsTruthy: true })
+  eleventyConfig.setLiquidOptions({ jsTruthy: true });
 
-  eleventyConfig.addCollection('articles', (collection) =>
-    collection.getFilteredByGlob('**/articles/*.md')
-  )
+  eleventyConfig.addCollection("articles", (collection) =>
+    collection.getFilteredByGlob("**/articles/*.md")
+  );
 
   return {
     dir: {
-      input: '_sources',
-      data: '_data'
-    }
-  }
+      input: "_sources",
+      data: "_data",
+    },
+  };
 }
